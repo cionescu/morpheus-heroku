49eb37c5d7527cc9b21214f834a23cec0bc92163
Catalin Ionescu <catalin.ionescu282@gmail.com>
2016-05-31 20:50:59 -0700

    [fix] Update the generator to use the new logo

--- lib/generators/morpheus_generator.rb
+++ lib/generators/morpheus_generator.rb
@@ -1,8 +1,8 @@
-class MorpheusGenerator < Rails::Generators::Base
+class MorpheusHerokuGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../", __FILE__)
 
   desc "Generate an initializer"
   def create_initializer_file
-    copy_file "config/morpheus-initializer.rb", "config/initializers/morpheus.rb"
+    copy_file "config/morpheus-heroku-initializer.rb", "config/initializers/morpheus-heroku.rb"
   end
 end
