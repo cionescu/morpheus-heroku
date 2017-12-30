cde05d695a064798e13db276433ca150da1e4029
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-12-30 21:31:33 +0200

    feat(loader): Drop active connections before dropping the db

--- lib/morpheus-heroku/load.rb
+++ lib/morpheus-heroku/load.rb
@@ -4,12 +4,23 @@ module MorpheusHeroku
 
     def run
       ensure_db_environment! if Rails.env.development?
+      drop_active_connections!
       drop_database!
       create_database!
       load_database!
     end
 
     private
+
+    def drop_active_connections!
+      sql_command = <<~SQL
+        SELECT pid, pg_terminate_backend(pid)
+        FROM pg_stat_activity
+        WHERE datname = current_database()
+          AND pid <> pg_backend_pid();
+      SQL
+      Helper.bash_run(command: "psql -d seriesreminder_development -c '#{sql_command}'")
+    end
 
     def ensure_db_environment!
       Helper.bash_run(command: "bundle exec rake db:environment:set RAILS_ENV=development")
