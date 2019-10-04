02cb11c55e0d3e81fa4f366a7d30586e2cbbe045
Rafael Cascalho <cascalhoeng@gmail.com>
2019-10-04 16:28:13 -0300

    bugfix: make sure of the folder structure to download the backup

--- lib/morpheus-heroku/fetch.rb
+++ lib/morpheus-heroku/fetch.rb
@@ -14,7 +14,28 @@ module MorpheusHeroku
     end
 
     def download_backup!
+      prepare_location()
       Helper.bash_run(command: "curl -o #{MorpheusHeroku.configuration.backup_location} `heroku pg:backups public-url`")
+    end
+
+    def prepare_location
+      location = MorpheusHeroku.configuration.backup_location
+      unless File.writable?(location)
+        prepare_dir(location[/^.+(?=\/)/])
+        prepare_file(location[/(?<=\/).+$/])
+      end
+    end
+
+    def prepare_dir(dir_name)
+      unless Dir.exist?(dir_name)
+        Helper.bash_run(command: "mkdir #{dir_name}")
+      end
+    end
+
+    def prepare_file(file_name)
+      unless File.exist?(file_name)
+        Helper.bash_run(command: "touch #{file_name}")
+      end
     end
   end
 end
